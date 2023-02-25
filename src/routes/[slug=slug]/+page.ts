f7a61412563c02a1671104ff479f4f65cd3504ab
Samya brata Maji <samyabratamaji334@gmail.com>
2023-02-25 23:48:13 +0530

    update content page

--- src/routes/[slug=slug]/+page.ts
+++ src/routes/[slug=slug]/+page.ts
@@ -11,6 +11,5 @@ export const load = (({ params }: { params: { slug: string } }) => {
 			return CONTENT_DATA[i];
 		}
 	}
-
-	// throw error(404, 'Not found');
+	throw error(404, 'Not found');
 }) satisfies PageLoad;
