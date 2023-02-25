8fcdac97309e17c97e5b4073be5bda3794b7a127
Samya brata Maji <samyabratamaji334@gmail.com>
2023-02-25 10:35:43 +0530

    add: types for cotent data (#4)

---
+++ src/data/content.data.ts
@@ -0,0 +1,14 @@
+type ContentPageType = {
+	title: string;
+	description: string;
+};
+
+type ContentType = {
+	pageOne: ContentPageType;
+	pageTwo: ContentPageType;
+	pageThree: ContentPageType;
+};
+
+export const CONTENT_DATA: ContentType[] = [
+	// add your data here
+];
